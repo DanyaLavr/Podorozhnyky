8c23cfa65dcb392fdc471521b3a2772442d9ac73
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-01-19 17:10:10 +0100

    add active status to btn component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,6 +7,7 @@ interface IProps {
   variant: TVariant;
   size: TSize;
   disabled?: boolean;
+  isActive?: boolean;
   onClick: MouseEventHandler<HTMLButtonElement>;
   children: ReactNode;
 }
@@ -14,37 +15,54 @@ const Button = ({
   variant,
   size,
   disabled = false,
+  isActive = false,
   onClick,
   children,
 }: IProps) => {
-  const paddings =
-    size === "big" ? "py-[10.5px] px-3 rounded-lg" : "py-1 px-2.5 rounded-md";
+  const paddings = size === "big" ? "py-[10.5px] px-3 " : "py-1 px-2.5 ";
   const disabledStyles = disabled
     ? "opacity-30 cursor-not-allowed"
     : "cursor-pointer";
   const styles =
     variant === "primary"
-      ? `text-gray-50 bg-blue-400 border-[1px] border-blue-400 
-      dark:text-gray-900 dark:bg-gray-50 dark:border-[1px] dark:border-gray-50
+      ? ` border border-blue-400 dark:border-gray-50 
+      ${
+        isActive
+          ? "text-gray-900 bg-blue-700 dark:active:text-gray-50 dark:bg-gray-500 dark:border-gray-400"
+          : "text-gray-50 bg-blue-400 dark:text-gray-900 dark:bg-gray-50"
+      } 
        ${
          !disabled
-           ? "hover:bg-blue-50 hover:text-gray-900 focus:text-gray-900 focus:bg-blue-50 active:bg-blue-700 dark:hover:bg-gray-400 dark:hover:text-gray-50 dark:focus:bg-gray-400 dark:focus:text-gray-50 dark:active:bg-gray-500 dark:active:text-gray-50 dark:active:border-gray-400"
+           ? `hover:bg-blue-50 hover:text-gray-900
+            focus:text-gray-900 focus:bg-blue-50
+            dark:hover:bg-gray-400 dark:hover:text-gray-50
+             dark:focus:bg-gray-400 dark:focus:text-gray-50`
            : ""
        }`
-      : `text-gray-900 bg-gray-50/5 border-[1px] border-gray-50/15 
-      dark:text-gray-50 dark:bg-gray-50/10 dark:border-none
+      : `
+       border border-gray-900/15 
+      ${
+        isActive
+          ? "bg-blue-900 text-gray-50 dark:text-gray-900 dark:bg-blue-700"
+          : "text-gray-900 bg-gray-900/5 dark:text-gray-50 dark:bg-gray-50/10 dark:border-none"
+      }
        ${
          !disabled
-           ? "hover:bg-blue-400 hover:text-gray-50 focus:bg-blue-400 focus:text-gray-50 active:bg-blue-900 active:text-gray-50 dark:hover:bg-blue-50 dark:hover:text-gray-900 dark:focus:bg-blue-50 dark:focus:text-gray-900 dark:active:text-gray-900 dark:active:bg-blue-700"
+           ? `hover:bg-blue-400 hover:text-gray-50
+            focus:bg-blue-400 focus:text-gray-50
+             
+            dark:hover:bg-blue-50 dark:hover:text-gray-900
+            dark:focus:bg-blue-50 dark:focus:text-gray-900
+            `
            : ""
        }
      `;
 
   return (
     <button
-      className={`font-main font-medium text-lg ${paddings} ${styles} ${disabledStyles}`}
+      className={`font-main font-medium text-lg rounded-lg ${paddings} ${styles} ${disabledStyles}`}
       disabled={disabled}
-      onClick={onClick}
+      onClick={disabled ? undefined : onClick}
     >
       {children}
     </button>
