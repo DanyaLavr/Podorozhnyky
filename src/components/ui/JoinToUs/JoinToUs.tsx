e5ce0b63195dbbd08f0d405fb322cede775bdc8d
Andrewlosk <andrijlosk30@gmail.com>
2026-01-22 19:27:09 +0200

    feat: start dev join section

--- src/components/ui/JoinToUs/JoinToUs.tsx
+++ src/components/ui/JoinToUs/JoinToUs.tsx
@@ -1,6 +1,4 @@
-import Container from "../Container";
 import Button from "../Button";
-import H2 from "../H2";
 
 import { createBem } from "@/utils/createBem";
 import styles from "./JoinToUs.module.css";
@@ -9,10 +7,10 @@ const bem = createBem("joinToUs", styles);
 
 export default function JoinToUs() {
   return (
-    <section>
-      <Container>
+    <section className="section">
+      <div className="container">
         <div className={bem()}>
-          <H2>Приєднуйтесь до нашої спільноти</H2>
+          <h2 className={bem("title")}>Приєднуйтесь до нашої спільноти</h2>
 
           <p className={bem("description")}>
             Долучайтеся до мандрівників, які діляться своїми історіями та
@@ -23,7 +21,7 @@ export default function JoinToUs() {
             Зареєструватися
           </Button>
         </div>
-      </Container>
+      </div>
     </section>
   );
 }
