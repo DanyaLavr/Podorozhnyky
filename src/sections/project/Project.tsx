e74f6b7bc2b95f43ccb98c7c573d22ea12404ee1
mykhailo1000 <mykhailokhomiak@gmail.com>
2026-02-02 21:28:27 +0300

    feat: added styles

--- src/sections/project/Project.tsx
+++ src/sections/project/Project.tsx
@@ -9,7 +9,6 @@ const Project = () => {
                 <div className={bem("title_desc")}>
                     <h2 className={bem("title")}>
                         Проєкт, створений для тих,
-                        <br />
                         хто живе подорожами
                     </h2>
                     <p className={bem("desc")}>
@@ -26,25 +25,25 @@ const Project = () => {
                         <img src="../../../public/afterHero/magic_stick.svg" className={bem("svg")}></img>
                         <h3 className={bem("title2")} > Наша місія</h3>
                         <p className={bem("desc2")}>
-                            Обʼєднувати людей через любов до природи та <br />
+                            Обʼєднувати людей через любов до природи та
                             надихати на нові відкриття.
                         </p>
                     </div>
 
-                    <div className="afterHero__mission">
+                    <div className={bem("mission")}>
                         <img src="../../../public/afterHero/suitcase.svg" className={bem("svg")}></img>
                         <h3 className={bem("title2")}>Автентичні історії</h3>
                         <p className={bem("desc2")}>
-                            Ми цінуємо справжні, неретушовані враження <br />
+                            Ми цінуємо справжні, неретушовані враження
                             від мандрівників з усього світу.
                         </p>
                     </div>
 
-                    <div className="afterHero__mission">
+                    <div className={bem("mission")}>
                         <img src="../../../public/afterHero/people.svg" className={bem("svg")}></img>
                         <h3 className={bem("title2")}>Ваша спільнота</h3>
                         <p className={bem("desc2")}>
-                            Станьте частиною спільноти, де кожен може <br />
+                            Станьте частиною спільноти, де кожен може
                             бути і автором, і читачем.
                         </p>
                     </div>
