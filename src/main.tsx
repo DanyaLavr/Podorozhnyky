0a58283ad9d30988d1d90dab0bfedaac52f2288c
VladIttop <129985238+VladIttop@users.noreply.github.com>
2026-02-12 17:29:17 +0000

    fix setupStore in main

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./styles/index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import Historia from "./components/Historia/Historia.tsx";
-import { store } from "./redux/store.ts";
+import { setupStore } from "./redux/store.ts";
 import { Provider } from "react-redux";
 import Footer from "./components/Footer/Footer.tsx";
 
